37867ff171a7f92dd8096d3dac9b156fb5f82e2c
Miguel Senger Naiba <04601730066@senacrs.edu.br>
2025-04-29 07:55:56 -0300

    commit

--- script.js
+++ script.js
@@ -1,23 +1,80 @@
 const produtos = [
+    // Macacos
     {
         titulo: "César",
         preco: "$ 259 Bananas",
-        imagem: "https://pbs.twimg.com/profile_images/1067870218456715264/XIWPNodw_400x400.jpg"
+        imagem: "https://pbs.twimg.com/profile_images/1067870218456715264/XIWPNodw_400x400.jpg",
+        categoria: "macacos"
     },
     {
         titulo: "Gorila",
         preco: "$ 999.99 Bananas",
-        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6DRlQ7dvuPD15NcWGHdU2qH7taprrSsIRLw&s"
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6DRlQ7dvuPD15NcWGHdU2qH7taprrSsIRLw&s",
+        categoria: "macacos"
     },
     {
         titulo: "Le Monke",
         preco: "$ 299 Bananas",
-        imagem: "https://static.wikia.nocookie.net/bbkcm/images/9/91/LeMonke.png/revision/latest?cb=20230827171036"
+        imagem: "https://static.wikia.nocookie.net/bbkcm/images/9/91/LeMonke.png/revision/latest?cb=20230827171036",
+        categoria: "macacos"
     },
     {
-        titulo: "monke",
+        titulo: "baixo",
         preco: "$ 999 Bananas",
-        imagem: "https://media.revide.com.br/cache/74/97/7497fce570a4f03d95cca60533936a6c.jpg" 
+        imagem: "https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/07/chimp-down-syndrome.jpg",
+        categoria: "macacos"
+    },
+
+    // Tigres
+    {
+        titulo: "blanco",
+        preco: "$ 90 Bananas",
+        imagem: "https://pilarmmaldonado.wordpress.com/wp-content/uploads/2013/02/e0899-tigre-albino.jpg",
+        categoria: "tigres"
+    },
+    {
+        titulo: "cartinha",
+        preco: "$ 20 Bananas",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTchjM_Kg-QQ_g_cD5C0Ronm-F3x50mErUAaA&s",
+        categoria: "tigres"
+    },
+    {
+        titulo: "volibear",
+        preco: "$ 127 Bananas",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXIYA3uXR0Y80rHzrwBYEu8Agwv-eYOj4o8w&s",
+        categoria: "tigres"
+    },
+    {
+        titulo: "diego",
+        preco: "$ 500 Bananas",
+        imagem: "https://static.wikia.nocookie.net/aeradogelo/images/9/9e/Lenny_%28Ice_Age%29.jpg/revision/latest?cb=20210619100625&path-prefix=pt-br",
+        categoria: "tigres"
+    },
+
+    // Onças
+    {
+        titulo: "tauba",
+        preco: "$ 29 Bananas",
+        imagem: "https://pm1.aminoapps.com/8631/cb78da692104cf02478597d1b53bcf8e655f66d3r1-1080-1054v2_hq.jpg",
+        categoria: "oncas"
+    },
+    {
+        titulo: "alisa",
+        preco: "$ 999 Bananas",
+        imagem: "https://pbs.twimg.com/profile_images/1728223238608621568/4fD4ln72_400x400.jpg",
+        categoria: "oncas"
+    },
+    {
+        titulo: "pintuda",
+        preco: "$ 919 Bananas",
+        imagem: "https://i.redd.it/gho0ll85dzhc1.jpeg",
+        categoria: "oncas"
+    },
+    {
+        titulo: "nega",
+        preco: "$ 099 Bananas",
+        imagem: "https://istoe.com.br/wp-content/uploads/2021/05/filhote-de-onca.jpg",
+        categoria: "oncas"
     }
 ];
 const produtosNaCesta = [];
@@ -146,6 +203,65 @@ document.addEventListener('click', (event) => {
 
 listaC.addEventListener('click', (event) => {
     event.stopPropagation(); 
+});
+
+function exibirProdutosPorCategoria(categoria) {
+    const nav = document.querySelector('nav');
+
+    container.innerHTML = "";
+    container.appendChild(nav); 
+
+    const produtosFiltrados = produtos.filter(produto => produto.categoria === categoria);
+
+    produtosFiltrados.forEach((produto, index) => {
+        const divProduto = document.createElement('div');
+        divProduto.classList.add('produto');
+        divProduto.innerHTML = `
+            <img class="produtomargin" src="${produto.imagem}" alt="${produto.titulo}">
+            <h1 class="titulo">${produto.titulo}</h1>
+            <h2 class="preco"><b>${produto.preco}</b></h2>
+            <button class="comprar" data-index="${index}">Adquirir</button>
+        `;
+        container.appendChild(divProduto);
+    });
+
+    const botoesComprar = document.querySelectorAll('.comprar');
+    botoesComprar.forEach(botao => {
+        botao.addEventListener('click', (e) => {
+            const index = e.target.getAttribute('data-index');
+            adicionarProdutoNaCesta(index);
+        });
+    });
+}
+
+document.getElementById('Macacos').addEventListener('click', () => exibirProdutosPorCategoria('macacos'));
+document.getElementById('Tigres').addEventListener('click', () => exibirProdutosPorCategoria('tigres'));
+document.getElementById('Onças').addEventListener('click', () => exibirProdutosPorCategoria('oncas'));
+document.getElementById('Todos').addEventListener('click', () => {
+
+    const nav = document.querySelector('nav');
+    container.innerHTML = "";
+    container.appendChild(nav);
+
+    produtos.forEach((produto, index) => {
+        const divProduto = document.createElement('div');
+        divProduto.classList.add('produto');
+        divProduto.innerHTML = `
+            <img class="produtomargin" src="${produto.imagem}" alt="${produto.titulo}">
+            <h1 class="titulo">${produto.titulo}</h1>
+            <h2 class="preco"><b>${produto.preco}</b></h2>
+            <button class="comprar" data-index="${index}">Adquirir</button>
+        `;
+        container.appendChild(divProduto);
+    });
+
+    const botoesComprar = document.querySelectorAll('.comprar');
+    botoesComprar.forEach(botao => {
+        botao.addEventListener('click', (e) => {
+            const index = e.target.getAttribute('data-index');
+            adicionarProdutoNaCesta(index);
+        });
+    });
 });
 
 
